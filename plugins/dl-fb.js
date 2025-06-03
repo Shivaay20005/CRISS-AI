084388a87584a882be617af7832bafa779ef87c3
Shivaay20005 <lava.blaze.5g.pro@gmail.com>
2025-06-03 08:25:42 +0530

    Update dl-fb.js

--- plugins/dl-fb.js
+++ plugins/dl-fb.js
@@ -31,7 +31,7 @@ cmd({
     const videoUrl = data.data.url;
     await conn.sendMessage(from, {
       video: { url: videoUrl },
-      caption: "📥 *Facebook Video Downloaded*\n\n- *Powered By Criss Vevo ✅*",
+      caption: "📥 *Facebook Video Downloaded*\n\n- *Powered By SHIVAAY  ✅*",
     }, { quoted: m });
 
   } catch (error) {
